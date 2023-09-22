656712ea1e67f3831b42dd001ca8b2e10de5efb8
BatorL1 <145106951+BatorL1@users.noreply.github.com>
2023-09-22 19:35:52 +0100

    Update Playing with code.js
    up to line 36

--- Playing with code.js
+++ Playing with code.js
@@ -1,1 +1,36 @@
+//Trying out code shown in class slides
 
+//var i, number_of_hits = 0, done = true, fruit = "yoghurt";
+//let ID = Symbol();
+//const pi = Math.PI;
+
+var a = 22;
+a *= 3;
+alert( a );
+//outputs 66 as an alert
+
+
+if ( num == 12) {
+    bool1 = true
+} else {
+    bool = 2
+}
+
+switch (num == 12 ) {
+    case true: bool1 = 1;
+    break;
+    case false: bool1 = 2;
+    break;
+    default: boo1=???;
+}
+
+//use switch cases instead of if else chains
+//switch case more compact
+
+
+//Keyword var declares variables with scope of function they’re in.
+//let variables can’t be redeclared in scope. var variables can.
+
+var car = { make: "Ford", model: "Focus", year: 2017 };
+
+//this is an object
